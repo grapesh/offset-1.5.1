178eebc79884d3317b516d5308b5d9875a1a3d4e
Sergey Vinogradov <grapesh@gmail.com>
2019-03-30 12:24:53 -0400

    exclude list added

--- scripts/compute_surface_atl.bash
+++ scripts/compute_surface_atl.bash
@@ -5,7 +5,9 @@
 #module load python/2.7.13
 
 export pyPath="/usrx/local/dev/python/2.7.13/bin"
+export pyPath="/Users/svinogra/anaconda/bin"
 export platform=""
+export platform="/Users/svinogra/mirrors/wcoss/surge"
 
 export myModules=${platform}"/gpfs/hps3/nos/noscrub/nwprod/csdlpy-1.5.1"
 export pythonCode=${platform}"/gpfs/hps3/nos/noscrub/nwprod/offset-1.5.1/offset/interpolate.py"
@@ -14,6 +16,7 @@ export logFile=${platform}"/gpfs/hps3/nos/noscrub/polar/offset/interp.atl.log"
 
 export NDAYS=${1:-2} # 2 days averaging by default. Change it to 1 to 7 if need be. 
 export inputFile=${platform}"/gpfs/hps3/nos/noscrub/polar/offset/latest_coops.csv"
+export excludeList=${platform}"/gpfs/hps3/nos/noscrub/polar/offset/exclude_stations.csv"
 export pltCfgFile=${platform}"/gpfs/hps3/nos/noscrub/nwprod/offset-1.5.1/scripts/config.plot.offset.atl.ini"
 export outputFile=${platform}"/gpfs/hps3/nos/noscrub/polar/offset/atl/estofs.atl.offset.63"
 export tmpDir=${platform}"/gpfs/hps3/nos/noscrub/tmp/offset/atl"
@@ -22,5 +25,5 @@ export ftpLogin="svinogradov@emcrzdm"
 export ftpPath="/home/www/polar/estofs/offset/"
 
 cd ${tmpDir}
-PYTHONPATH=${myModules} ${pyPath}/python -W ignore ${pythonCode} -d ${NDAYS} -i ${inputFile} -o ${outputFile} -c ${pltCfgFile} -t ${tmpDir} -u ${ftpLogin} -f ${ftpPath} > ${logFile}
+PYTHONPATH=${myModules} ${pyPath}/python -W ignore ${pythonCode} -d ${NDAYS} -i ${inputFile} -e ${excludeList} -o ${outputFile} -c ${pltCfgFile} -t ${tmpDir} -u ${ftpLogin} -f ${ftpPath} # > ${logFile}
 
