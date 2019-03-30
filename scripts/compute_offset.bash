c0f4e8e5a8d9d5128a0f823adcdd6f3d351a3b6f
Sergey Vinogradov <grapesh@gmail.com>
2019-03-30 13:30:20 -0400

    Update compute_offset.bash

--- scripts/compute_offset.bash
+++ scripts/compute_offset.bash
@@ -8,21 +8,19 @@
 export pyPath="/usrx/local/dev/python/2.7.13/bin"
 export platform=""
 
-export myModules=${platform}"/gpfs/hps3/nos/noscrub/Sergey.Vinogradov/IT-hsofs.v2.0.2/lib/csdlpy-1.5.1"
-export pythonCode=${platform}"/gpfs/hps3/nos/noscrub/Sergey.Vinogradov/IT-hsofs.v2.0.2/ush/offset-1.5.1/offset/compute.py"
+export myModules=${platform}"/gpfs/hps3/nos/noscrub/nwprod/csdlpy-1.5.1"
+export pythonCode=${platform}"/gpfs/hps3/nos/noscrub/nwprod/offset-1.5.1/offset/compute.py"
 export logFile=${platform}"/gpfs/hps3/nos/noscrub/polar/offset/offset.log"
 
 #export dataTankPath="web"
-#export dataTankPath=${platform}"/gpfs/gp1/dcomdev/us007003/"
-export dataTankPath=${platform}"/gpfs/hps3/nos/noscrub/Sergey.Vinogradov/us007003/"
-#export endDate="20190217"
-export endDate="latest"
+export dataTankPath=${platform}"/gpfs/tp1/dcomdev/us007003/"
+export endDate="latest" #"20190330"
 
-export outputFile=${platform}"/gpfs/hps3/nos/noscrub/Sergey.Vinogradov/IT-hsofs.v2.0.2/latest_coops.csv"
-export tmpDir=${platform}"/gpfs/hps3/nos/noscrub/Sergey.Vinogradov/tmp/"
+export outputFile=${platform}"/gpfs/hps3/nos/noscrub/polar/offset/latest_coops.csv"
+export tmpDir=${platform}"/gpfs/hps3/nos/noscrub/tmp/offset/atl/"
 
-#export ftpLogin="svinogradov@emcrzdm"
-#export ftpPath="/home/ftp/polar/estofs/offset/"
+export ftpLogin="svinogradov@emcrzdm"
+export ftpPath="/home/www/polar/estofs/offset/"
 
 cd ${tmpDir}
-PYTHONPATH=${myModules} ${pyPath}/python -W ignore ${pythonCode} -z ${endDate} -o ${outputFile} -t ${tmpDir} -p ${dataTankPath} # -u ${ftpLogin} -f ${ftpPath} #> ${logFile}
+PYTHONPATH=${myModules} ${pyPath}/python -W ignore ${pythonCode} -z ${endDate} -o ${outputFile} -t ${tmpDir} -p ${dataTankPath} -u ${ftpLogin} -f ${ftpPath} > ${logFile}
